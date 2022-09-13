83aad0309805205a6f697e61a39e77003d0a475c
bekirugurr <bekirugur070@gmail.com>
2022-09-13 19:28:10 +0300

    layout of skills page rearranged

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -58,10 +58,10 @@ const Skills = ({setActiveItem}) => {
     }
   },[inView, animationImage, animationText, setActiveItem]);
 
-  let tools =[['HTML5',HTML5], ['CSS3', CSS3], ['Javascript',Javascript], ['React',React], ['Redux',Redux], ['Tailwind',Tailwind], ['jQuery',jQuery], ['Bootstrap',Bootstrap], ['Python',Python], ['django',django], ['Firebase',Firebase], ['Formik',Formik], ['Github',Github], ['Material UI',MaterialUI], ['PostgreSQL',PostgreSQL], ['SQLite',SQLite],  ['Heroku',Heroku], ['Styled',StyledComponent]]  
+  let tools =[['HTML5',HTML5], ['CSS3', CSS3], ['Javascript',Javascript], ['React',React], ['Redux',Redux], ['Tailwind',Tailwind], ['jQuery',jQuery], ['Bootstrap',Bootstrap], ['Python',Python], ['django',django], ['Firebase',Firebase], ['Formik',Formik], ['Github',Github], ['MaterialUI',MaterialUI], ['SQLite', SQLite], ['PostgreSQL',PostgreSQL],  ['Heroku',Heroku], ['Styled',StyledComponent]]  
 
   return (
-    <div ref={ref} id="skills" className="min-h-screen font-body bg-green-200 px-0 mx-0 pt-16 px-1 md:px-4 lg:px-12 pb-12 md:pb-24">
+    <div ref={ref} id="skills" className="min-h-screen font-body bg-green-200 sm:bg-red-300 md:bg-blue-300 lg:bg-yellow-300 xl:bg-white px-0 mx-0 pt-16 px-1 md:px-4 lg:px-12 pb-12 md:pb-24">
       <h1 className="text-4xl font-bold text-gray-800 mt-4 text-center">Skillset</h1>
       <h3 className="text-2xl font-bold text-gray-800 mt-4 text-center">
         Languages, libraries, frameworks and tools I use in development:
@@ -69,32 +69,32 @@ const Skills = ({setActiveItem}) => {
       <ul className="flex flex-row flex-wrap px-4 lg:px-24  mt-4 md:mt-10 mb-0 md:mb-2 overflow-hidden">
 
         {tools.map((item)=> 
-        (<li key={item[1]} className="flex flex-col items-center my-2 mx-3 my-3 md:mx-6 ">
+        (<li key={item[1]} className="flex flex-col items-center  mx-3 my-3 md:mx-6 pb-6 relative">
           <motion.div className="w-16 h-16 p-2 lg:w-20 lg:h-20 lg:p-3 rounded-full bg-gray-200 border-solid border-2 border-gray-400" animate={animationImage}>
             <img src={item[1]} alt={item[0]}/>
           </motion.div>
-          <motion.p className="text-gray-700" animate={animationText}>{item[0]}</motion.p>
+          <motion.p className="text-gray-700 absolute bottom-0" animate={animationText}>{item[0]}</motion.p>
         </li>)
          )} 
         {/*NOTE: Since their sizes and spaces are different, the ones below were written one by one.*/}
        
-        <li className="flex flex-col items-center my-2 mx-3 md:my-3 md:mx-6">
+        <li className="flex flex-col items-center my-2 mx-3 md:my-3 md:mx-6 pb-6 relative">
           <motion.div className="w-16 h-16 py-2 pl-1 pr-3 lg:w-20 lg:h-20 lg:py-3 md:pl-2 lg:pr-4 rounded-full bg-gray-200 border-solid border-2 border-gray-400" animate={animationImage}>
             <img src={postman} alt="Postman"  />
           </motion.div>
-          <motion.p className="text-gray-700" animate={animationText}>Postman</motion.p>
+          <motion.p className="text-gray-700 absolute bottom-0" animate={animationText}>Postman</motion.p>
         </li>
-        <li className="flex flex-col items-center my-2 mx-3 md:my-3 md:mx-6 ">
+        <li className="flex flex-col items-center my-2 mx-3 md:my-3 md:mx-6 pb-6 relative">
           <motion.div className="w-16 h-16 py-2 pl-3 pr-1 lg:w-20 lg:h-20 lg:py-3 md:pl-4 lg:pr-2 rounded-full bg-gray-200 border-solid border-2 border-gray-400" animate={animationImage}>
             <img src={sass} alt="sass" />
           </motion.div>
-          <motion.p className="text-gray-700" animate={animationText}>Sass</motion.p>
+          <motion.p className="text-gray-700 absolute bottom-0" animate={animationText}>Sass</motion.p>
         </li>
-        <li className="flex flex-col items-center my-2 mx-3 md:my-3 md:mx-6 ">
+        <li className="flex flex-col items-center my-2 mx-3 md:my-3 md:mx-6 pb-6 relative">
           <motion.div className="w-16 h-16 py-3 pl-2 pr-4 lg:w-20 lg:h-20 lg:py-4 md:pl-3 lg:pr-5 rounded-full bg-gray-200 border-solid border-2 border-gray-400" animate={animationImage}>
             <img src={vscode} alt="VS Code" />
           </motion.div>
-          <motion.p className="text-gray-700" animate={animationText}>VS Code</motion.p>
+          <motion.p className="text-gray-700 absolute bottom-0" animate={animationText}>VSCode</motion.p>
         </li>
       </ul>
     </div>
